95ffa3743dad448fc00420943d7e35e34a0f6c81
Aditya Aneja <aditya.aneja@gmail.com>
2018-01-24 15:37:49 -0700

    version 1.0.0 for sample chef_server on AWS

---
+++ vars.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "The AWS region to use"
+  default     = "us-east-1"
+}
+
+variable "aws_vpc" {
+  description = "The name of the vpc to use "
+  default     = "vpc-72fc5d0a"
+}
+
+variable "aws_az" {
+  description = "The name of the AZ to use "
+  default     = "us-east-1a"
+}
+/*
+variable "aws_subnetid" {
+  description = "The name of the AZ to use "
+  default     = "subnet-db26cef4"
+}
+*/
+
+variable "ec2_instance_type" {
+  description = "The instance type for EC2 "
+  default     = "t2.medium"
+}
+
+
